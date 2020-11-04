22a52bc14ec6a34462273d2593b9b3e0ad6015f5
Shreyas Daniel <iamshreydan@gmail.com>
2020-11-04 19:16:03 +0530

    added 1x2 bounding box

--- picker.py
+++ picker.py
@@ -4,10 +4,12 @@ import pyscreenshot as ImageGrab
 from PIL import Image
 
 def checkColor(xy):
-    im = ImageGrab.grab()
+    boundingbox = (xy[0],xy[1],xy[0]+1,xy[1]+1)
+    im = ImageGrab.grab(bbox=boundingbox)
     rgbim = im.convert('RGB')
-    r,g,b = rgbim.getpixel(xy)
+    r,g,b = rgbim.getpixel((0,0))
     print('rgb value: ',(r,g,b))
+    
 
 def onClick(x,y, button, pressed):
 
