13c8b7d2eaa5d80bc39f9ddd6e670e4b8633cc61
Shreyas Daniel <iamshreydan@gmail.com>
2021-04-12 21:54:57 +0530

    added hexcode for colors

--- picker.py
+++ picker.py
@@ -3,12 +3,17 @@ from pynput.mouse import Button as MouseButton
 import pyscreenshot as ImageGrab
 from PIL import Image
 
+def getHex(rgb):
+    return '%02X%02X%02X'%rgb
+
+
 def checkColor(xy):
     bbox = (xy[0],xy[1],xy[0]+1,xy[1]+1)
     im = ImageGrab.grab(bbox=bbox)
     rgbim = im.convert('RGB')
     r,g,b = rgbim.getpixel((0,0))
     print('rgb value: ',(r,g,b))
+    print('hex value: #',getHex((r,g,b)))
     
 
 def onClick(x,y, button, pressed):
