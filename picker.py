d992d620860128cd6286281fff87066914310451
Shreyas Daniel <iamshreydan@gmail.com>
2021-05-22 21:40:12 +0530

    removed pyscreenshot, added better interaction
    1. pyscreenshot is obsolete, ImageGrab from Pillow is used instead.
    2, removed right click to exit and replaced with keyboard Esc key

--- picker.py
+++ picker.py
@@ -1,31 +1,33 @@
-from pynput.mouse import Listener as MouseListener
-from pynput.mouse import Button as MouseButton
-import pyscreenshot as ImageGrab
-from PIL import Image
+from pynput import keyboard
+from pynput import mouse
+from PIL import Image, ImageGrab
 
 def getHex(rgb):
     return '%02X%02X%02X'%rgb
 
 
-def checkColor(xy):
-    bbox = (xy[0],xy[1],xy[0]+1,xy[1]+1)
+def checkColor(x,y):
+    bbox = (x,y,x+1,y+1)
     im = ImageGrab.grab(bbox=bbox)
     rgbim = im.convert('RGB')
     r,g,b = rgbim.getpixel((0,0))
-    print('rgb value: ',(r,g,b))
-    print('hex value: #',getHex((r,g,b)))
+    print(f'COLOR: rgb{(r,g,b)} | HEX #{getHex((r,g,b))}')
     
-
+   
 def onClick(x,y, button, pressed):
+	if pressed and button == mouse.Button.left:
+		checkColor(x,y)
 
-    if pressed and button == MouseButton.left:
-        checkColor((x,y))
-
-    if pressed and button == MouseButton.right:
-        return False
-
+def onRel(key):
+	if key == keyboard.Key.esc:
+		mlstnr.stop()
+		return False
 
 
-m = MouseListener(on_click=onClick)
-m.start()
-m.join()
+
+if __name__ == '__main__':
+    with keyboard.Listener(on_release = onRel) as klstnr:
+        with mouse.Listener(on_click = onClick) as mlstnr:
+            klstnr.join()
+            mlstnr.join()
+
